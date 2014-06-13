e168f0a7aba775f041666281fb46f7e4e6b5b694
Elliot Kroo <elliot@getaround.com>
2014-06-13 11:57:29 -0700

    Initial Commit

---
+++ Blueforce/Blueforce.m
@@ -0,0 +1,13 @@
+//
+//  Blueforce.m
+//  Blueforce
+//
+//  Created by Elliot Kroo on 6/13/14.
+//  Copyright (c) 2014 Getaround. All rights reserved.
+//
+
+#import "Blueforce.h"
+
+@implementation Blueforce
+
+@end
